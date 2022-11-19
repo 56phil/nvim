422baaa3be4744f06cc2c263a683c80da1805b17
Phil Huffman <56phil@users.noreply.github.com>
2022-11-19 10:05:57 -0600

    Add files via upload

---
+++ lua/phil/core/keymaps.lua
@@ -0,0 +1,76 @@
+-- set leader key to space
+vim.g.mapleader = " "
+
+local keymap = vim.keymap -- for conciseness
+
+---------------------
+-- General Keymaps
+---------------------
+
+-- use jk to exit insert mode
+keymap.set("i", "jk", "<ESC>")
+
+-- clear search highlights
+keymap.set("n", "<leader>nh", ":nohl<CR>")
+
+-- delete single character without copying into register
+keymap.set("n", "x", '"_x')
+
+-- increment/decrement numbers
+keymap.set("n", "<leader>+", "<C-a>") -- increment
+keymap.set("n", "<leader>-", "<C-x>") -- decrement
+
+-- window management
+keymap.set("n", "<leader>sv", "<C-w>v") -- split window vertically
+keymap.set("n", "<leader>sp", "<C-w>s") -- split window prone (horizontally)
+keymap.set("n", "<leader>se", "<C-w>=") -- make split windows equal width & height
+keymap.set("n", "<leader>sw", ":close<CR>") -- close current split window
+keymap.set("n", "<leader>sk", ":wincmd k<CR>") -- next window up
+keymap.set("n", "<leader>sj", ":wincmd j<CR>") -- next window down
+keymap.set("n", "<leader>sl", ":wincmd l<CR>") -- next window right
+keymap.set("n", "<leader>sh", ":wincmd h<CR>") -- next window left
+
+-- tab management
+keymap.set("n", "<leader>tn", ":tabnew<CR>") -- open new tab
+keymap.set("n", "<leader>tw", ":tabclose<CR>") -- close current tab
+keymap.set("n", "<leader>tl", ":tabn<CR>") --  go to next tab
+keymap.set("n", "<leader>th", ":tabp<CR>") --  go to previous tab
+
+-- line mover
+keymap.set("n", "<leader>lk", "ddkP") -- move line up
+keymap.set("n", "<leader>lj", "ddp") -- move line down
+
+-- buffer navagation
+keymap.set("n", "<leader>bl", ":bn<cr>") -- next buffer
+keymap.set("n", "<leader>bh", ":bp<cr>") -- previous buffer
+
+-- miscellaneous mappings
+keymap.set("n", "<leader>ws", ":w<CR>:luafile %<CR>") --  write & source
+keymap.set("n", "<leader>ch", ":checkhealth<CR>") --  run checkhealth
+keymap.set("n", "<leader>ps", ":PackerSync<CR>") --  make plugins current
+
+----------------------
+-- plugin eybinds
+----------------------
+
+-- vim-maximizer
+keymap.set("n", "<leader>sm", ":MaximizerToggle<CR>") -- toggle split window maximization
+
+-- nvim-tree
+keymap.set("n", "<leader>e", ":NvimTreeToggle<CR>") -- toggle file explorer
+
+-- telescope
+keymap.set("n", "<leader>ff", "<cmd>Telescope find_files<cr>") -- find files within current working directory, respects .gitignore
+keymap.set("n", "<leader>fs", "<cmd>Telescope live_grep<cr>") -- find string in current working directory as you type
+keymap.set("n", "<leader>fc", "<cmd>Telescope grep_string<cr>") -- find string under cursor in current working directory
+keymap.set("n", "<leader>fb", "<cmd>Telescope buffers<cr>") -- list open buffers in current neovim instance
+keymap.set("n", "<leader>fh", "<cmd>Telescope help_tags<cr>") -- list available help tags
+
+-- telescope git commands (not on youtube nvim video)
+keymap.set("n", "<leader>gc", "<cmd>Telescope git_commits<cr>") -- list all git commits (use <cr> to checkout) ["gc" for git commits]
+keymap.set("n", "<leader>gfc", "<cmd>Telescope git_bcommits<cr>") -- list git commits for current file/buffer (use <cr> to checkout) ["gfc" for git file commits]
+keymap.set("n", "<leader>gb", "<cmd>Telescope git_branches<cr>") -- list git branches (use <cr> to checkout) ["gb" for git branch]
+keymap.set("n", "<leader>gs", "<cmd>Telescope git_status<cr>") -- list current changes per file with diff preview ["gs" for git status]
+
+-- restart lsp server (not on youtube nvim video)
+keymap.set("n", "<leader>rs", ":LspRestart<CR>") -- mapping to restart lsp if necessary
